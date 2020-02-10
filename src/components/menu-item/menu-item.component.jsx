b1b8b2a79220adcc5bcb923cc8d9ea06d7d79294
Rakesh Jangir <rjtarpura@gail.com>
2020-02-11 01:00:27 +0530

    broken homepage in smaller components, finished styling our menu-item component.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,9 +3,10 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size})=>{
     return (
-        <div className={`${size} menu-item`} style={{
-            backgroundImage: `url(${imageUrl})` //javascript template string with string interpolation
-        }}>
+        <div className={`${size} menu-item`}>
+            <div className="background-image" style={{
+                backgroundImage: `url(${imageUrl})` //javascript template string with string interpolation
+            }}/>
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">SHOP NOW</span>
