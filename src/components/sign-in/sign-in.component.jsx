61dde78c414c3b69387a0e82c380060ec98dbd70
Rakesh Jangir <rakeshj@ehealth.local>
2020-02-11 20:00:25 +0530

    SignIn component created

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+        };
+    }
+
+    handleChange = event=>{
+        const { name, value } = event.target;
+        this.setState({[name]:value});
+    }
+    handleSubmit = event=>{
+        event.preventDefault();
+        this.setState({email:'',password:''});
+    }
+
+    render(){
+        return(
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required/>
+                    <label>Email</label>
+                    <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required/>
+                    <label>Password</label>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SignIn;
