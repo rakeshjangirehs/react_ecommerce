9628b5f509e038cbb5986640a12bff35d7f0f994
Rakesh Jangir <rjtarpura@gail.com>
2020-02-13 01:30:35 +0530

    Store google logedin user data in our own user collection.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,30 @@ const firebaseConfig = {
     measurementId: "G-XV2HPGCCB0"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData)=>{
+
+    if(!userAuth) return;
+    const userRef = firestore.doc(`/users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+
+        const { displayName, email } = userAuth;
+        const createAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName, email, createAt, ...additionalData
+            });
+        } catch(error) {
+            console.log('error creating user', error.message)
+        }
+        userRef.set();
+    }
+    
+    return userRef;
+}
+
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
