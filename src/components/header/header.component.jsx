06a309c0985744b3e907d716ffffcd2d132f16b0
Rakesh Jangir <rjtarpura@gail.com>
2020-02-15 17:26:37 +0530

    ues redux in header.component.jsx to manage state of current user.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';  //HOC
 
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 import { auth } from '../../firebase/firebase.utils';
+
+
 
 const Header = ({ currentUser })=>{
     return(
@@ -26,4 +29,8 @@ const Header = ({ currentUser })=>{
     );
 }
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
