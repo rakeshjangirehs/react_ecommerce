4f002581c0ef9b5a0c7bb47de3c661b5725067cd
Rakesh Jangir <rjtarpura@gail.com>
2020-02-17 00:15:37 +0530

    Updates

---
+++ precache-manifest.e051ac9b8e16a0d8d7a1da7b985e44ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f78f0dd1dde3a9d580f4b5a508f8a80",
+    "url": "/react_ecommerce/index.html"
+  },
+  {
+    "revision": "314259740d30a2165e0b",
+    "url": "/react_ecommerce/static/css/main.c7e7c09c.chunk.css"
+  },
+  {
+    "revision": "8ba6845c45d8d9b30083",
+    "url": "/react_ecommerce/static/js/2.6d56b121.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/react_ecommerce/static/js/2.6d56b121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "314259740d30a2165e0b",
+    "url": "/react_ecommerce/static/js/main.de7b1204.chunk.js"
+  },
+  {
+    "revision": "b963a581ec2f44568529",
+    "url": "/react_ecommerce/static/js/runtime-main.0b4338d5.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/react_ecommerce/static/media/logo.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/react_ecommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
