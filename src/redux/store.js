edf0f1ff48e422bb328f8b5466a3635849f82867
Rakesh Jangir <rjtarpura@gail.com>
2020-02-16 23:57:35 +0530

    Install react-persist
    react-persist stores our cart in local storage and fetch it again at
    page reload.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
+
 import { logger } from 'redux-logger';
 
 import rootReducter from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducter,applyMiddleware(...middlewares));
+export const store = createStore(rootReducter,applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {store, persistor};
