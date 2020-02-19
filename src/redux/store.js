9034ffd7d09b24960bd8d73919d86ab03ce3d710
Rakesh Jangir <rjtarpura@gail.com>
2020-02-20 01:29:19 +0530

    logger is disabled for live server using node NODE_ENV variable

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,11 @@ import { logger } from 'redux-logger';
 
 import rootReducter from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducter,applyMiddleware(...middlewares));
 
