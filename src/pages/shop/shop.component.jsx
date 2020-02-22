124e4898e279036b263508fbcaf0b3ab9d3f3a84
Rakesh Jangir <rjtarpura@gail.com>
2020-02-23 01:03:09 +0530

    fetch collection data in shop.coponent

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { updateCollections } from '../../redux/shop/shop.actions';
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match }) => (
-    <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route exact path={`${match.path}/:collectionID`} component={CollectionPage} />
+class ShopPage extends React.Component{
+    
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount = () => {
         
-    </div>
-);
-export default ShopPage;
+        const { updateCollections } = this.props;
+
+        const collectionRef = firestore.collection('collections');
+
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async (snapshot) => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
+        });
+    }
+
+    componentWillUnmount = () => {
+
+    }
+
+    render(){
+
+        const { match } = this.props;
+        return (
+            <div className="shop-page">
+                <Route exact path={`${match.path}`} component={CollectionsOverview} />
+                <Route exact path={`${match.path}/:collectionID`} component={CollectionPage} />        
+            </div>
+        );
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => 
+        dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null,mapDispatchToProps)(ShopPage);
