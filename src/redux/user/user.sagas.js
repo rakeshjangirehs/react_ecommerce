6767cc6172d4fd19765db4a0d88a2d035c53025d
Rakesh Jangir <rjtarpura@gail.com>
2020-02-28 01:45:19 +0530

    Code refector in user.saga.js

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -5,12 +5,20 @@ import userActionTypes from './user.types';
 import {auth, googleProvider, createUserProfileDocument } from '../../firebase/firebase.utils';
 import { signInSuccess, signInFailure } from './user.actions';
 
+export function* getSnapshotFromUserAuth(userAuth) {
+    try {
+        const userRef = yield call(createUserProfileDocument, userAuth);
+        const userSnapshot = yield userRef.get();
+        yield put(signInSuccess({id: userSnapshot.id, ...userSnapshot.data()}));
+    } catch(error) {
+        yield put(signInFailure(error));
+    }
+}
+
 export function* signInWithGoogle() {
     try {
         const { user } = yield auth.signInWithPopup(googleProvider);
-        const userRef = yield call(createUserProfileDocument, user);
-        const userSnapshot = yield userRef.get();
-        yield put(signInSuccess({id: userSnapshot.id, ...userSnapshot.data()}));
+        yield getSnapshotFromUserAuth(user);
     } catch(error) {
         yield put(signInFailure(error));
     }
@@ -20,9 +28,7 @@ export function* signInWithEmail({ payload: { email, password} }) {
     try {
         const { user } = yield auth.signInWithEmailAndPassword(email, password);
 
-        const userRef = yield call(createUserProfileDocument, user);
-        const userSnapshot = yield userRef.get();
-        yield put(signInSuccess({id: userSnapshot.id, ...userSnapshot.data()}));
+        yield getSnapshotFromUserAuth(user);
 
     } catch(error) {
         yield put(signInFailure(error));
